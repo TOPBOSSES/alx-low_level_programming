4fbc10237debee74338ddbac87851c96ed57a3fb
TOPBOSSES <ezekielebubechukwu@gmail.com>
2022-03-13 05:25:36 -0700

    my forth commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,12 +6,12 @@
  * main - prints the digit of the random
  * number stored in the variable n
  * Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 	int n;
 
-	srand(time(0));
+	srand(time(0);
 	n = rand () - RAND_MAX / 2;
 	printf("Last digit of %d is %d ", n, n % 10);
 	if (n % 10 > 5)
