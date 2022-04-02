22badb26a7773b52e588285a26ae8efd632a76d2
TOPBOSSES <ezekielebubechukwu@gmail.com>
2022-04-02 05:39:46 -0700

    functions commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,32 +1,15 @@
 #include "main.h"
 
-int actual_prime(int n, int i);
-
 /**
- * is_prime_number - says if an integer is a prime number or not
- * @n: number to evaluate
- *
- * Return: 1 if n is a prime number, 0 if not
+ * print_line - prints a line with the length being the input
+ * @n: the length of the line
+ * Return: void
  */
-int is_prime_number(int n)
+void print_line(int n)
 {
-	if (n <= 1)
-		return (0);
-	return (actual_prime(n, n - 1));
-}
+	int i;
 
-/**
- * actual_prime - calculates if a number is prime recursively
- * @n: number to evaluate
- * @i: iterator
- *
- * REturn: 1 if n is prime, 0 if not
- */
-int actual_prime(int n, int i)
-{
-	if (i == 1)
-		return (1);
-	if (n % i == 0 && i > 0)
-		return (0);
-	return (actual_prime(n, i - 1));
+	for (i = 0; i < n; ++i)
+		_putchar('_');
+	_putchar('\n');
 }
