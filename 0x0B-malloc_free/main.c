a72c9fb3614628c7f6b89be7c87e42e07e6f7227
TOPBOSSES <ezekielebubechukwu@gmail.com>
2022-04-04 08:59:16 -0700

    malloc commit

--- 0x0B-malloc_free/main.c
+++ 0x0B-malloc_free/main.c
@@ -1,7 +1,23 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
-    return (0);
+	char *s;
+
+	s = _strdup("Holberton");
+	if (s == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	printf("%s\n", s);
+	free(s);
+	return (0);
 }
