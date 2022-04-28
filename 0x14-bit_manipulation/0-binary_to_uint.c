bc44ed66a97be1d462f1e61d5c4011eb7d591397
TOPBOSSES <ezekielebubechukwu@gmail.com>
2022-04-28 12:07:21 -0700

    bit commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,12 +2,12 @@
 
 
 /**
- * binary_to_unit - converts a binary number to unsiggned int
+ * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
  * Return: the converted number
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int dec_val = 0;
